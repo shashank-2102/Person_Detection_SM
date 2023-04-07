1a72f70f247ed2fa562f6b51aab553d6cd321cd9
shashank-2102 <shashank.prabhu21@gmail.com>
2023-04-08 00:07:07 +0200

    Added load model and get video functions

--- YOLOv5.py
+++ YOLOv5.py
@@ -21,10 +21,28 @@ class ObjectDetection:
         self.model = self.load_model()
         self.classes = self.model.names
         self.out_file = out_file
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu' #checks if cuda is available and uses it if it is
         print("\n \nDevice Used (if its not cuda gl)", self.device)
 
 
+    def get_video_from_url(self):
+        """
+        Generates video streaming object. Frame by frame extraction will be done in order to make predictions
+        :return: openCV2 video capture object, with lowest quality frame available for video
+        """
+
+        play = pafy.new(self._URL).streams[-1]
+        assert play is not None #makes sure play is not none
+        return cv2.VideoCapture(play.url) #here you can also give camera input (verify)
+    
+    def load_model(self):
+        """
+        Loads YOLO V5 Model from PyTorch
+        :return: Train model from PyTorch
+        """
+
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+        return model
     
 
 
